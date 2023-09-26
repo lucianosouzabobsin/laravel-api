bd73bcf31891e5956797ebcd19a654a38b63d5e2
Luciano Bobsin <lucianosouzabobsin@gmail.com>
2023-09-25 23:25:18 -0300

    Testes UserController

--- api/app/Http/Controllers/UserController.php
+++ api/app/Http/Controllers/UserController.php
@@ -23,24 +23,17 @@ class UserController extends Controller
      */
     public function register(Request $request)
     {
-        try {
-            $validator = Validator::make($request->all(), [
-                'name' => 'required|string|max:255',
-                'email' => 'required|string|email|unique:users|max:255',
-                'password' => 'required|string|min:8',
-            ]);
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users|max:255',
+            'password' => 'required|string|min:8',
+        ]);
 
-            if ($validator->fails()) {
-                return response()->json(['errors' => $validator->errors()], 422);
-            }
-
-            return $this->authUserService->register($request);
-
-        } catch (\Throwable $th) {
-            return response()->json([
-                'error' =>  'Bad request'
-            ], 404);
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
         }
+
+        return $this->authUserService->register($request);
     }
 
     /**
@@ -51,21 +44,15 @@ class UserController extends Controller
      */
     public function login(Request $request)
     {
-        try {
-            $credentials = [
-                'email' => $request->email,
-                'password' => $request->password
-            ];
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
 
-            if (auth()->attempt($credentials)) {
-                return $this->authUserService->login();
-            } else {
-                return response()->json(['error' => 'UnAuthorised'], 401);
-            }
-        } catch (\Throwable $th) {
-            return response()->json([
-                'error' =>  'Bad request'
-            ], 404);
+        if (auth()->attempt($credentials)) {
+            return $this->authUserService->login();
+        } else {
+            return response()->json(['error' => 'UnAuthorised'], 401);
         }
     }
 
---
+++ api/tests/Feature/Controller/UserControllerTest.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace Tests\Feature\Controller;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Factory;
+use Illuminate\Support\Facades\Hash;
+
+class UserControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Testa o registro de um novo usuário.
+     *
+     * @return void
+     */
+    public function testRegisterUser()
+    {
+        $response = $this->postJson('/api/register', [
+            'name' => 'John Doe',
+            'email' => 'john.doe@example.com',
+            'password' => 'password123',
+        ]);
+
+        $response->assertStatus(201);
+    }
+
+    /**
+     * Testa o registro de um novo usuário.
+     *
+     * @return void
+     */
+    public function testRegisterUserErrorValidatorNoName()
+    {
+        $response = $this->postJson('/api/register', [
+            'email' => 'john.doe@example.com',
+            'password' => 'password123',
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    /**
+     * Testa o registro de um novo usuário.
+     *
+     * @return void
+     */
+    public function testRegisterUserErrorValidatorNoPassword()
+    {
+        $response = $this->postJson('/api/register', [
+            'email' => 'john.doe@example.com',
+            'password' => '',
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    /**
+     * Testa o registro de um novo usuário.
+     *
+     * @return void
+     */
+    public function testRegisterUserErrorValidatorBelowStringMinimumPassword()
+    {
+        $response = $this->postJson('/api/register', [
+            'email' => 'john.doe@example.com',
+            'password' => '1234567',
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    /**
+     * Testa o registro de um novo usuário.
+     *
+     * @return void
+     */
+    public function testRegisterUserErrorValidatorUniqueEmail()
+    {
+        $response = $this->postJson('/api/register', [
+            'name' => 'John Doe',
+            'email' => 'john.doe@example.com',
+            'password' => 'password123',
+        ]);
+
+        $response = $this->postJson('/api/register', [
+            'name' => 'John Doe 2',
+            'email' => 'john.doe@example.com',
+            'password' => 'password1234666',
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+
+    /**
+     * Testa o login de um usuário existente.
+     *
+     * @return void
+     */
+    public function testLoginUser()
+    {
+        $response = $this->postJson('/api/register', [
+            'name' => 'Jane Doe',
+            'email' => 'jane.doe@example.com',
+            'password' => 'password456',
+        ]);
+
+        $response = $this->postJson('/api/login', [
+            'email' => 'jane.doe@example.com',
+            'password' => 'password456',
+        ]);
+
+        $response->assertStatus(201);
+    }
+
+    /**
+     * Testa o login de um usuário não autorizado.
+     *
+     * @return void
+     */
+    public function testLoginUserUnauthorised()
+    {
+        $response = $this->postJson('/api/register', [
+            'name' => 'Jane Doe',
+            'email' => 'jane.doe@example.com',
+            'password' => 'password456',
+        ]);
+
+        $response = $this->postJson('/api/login', [
+            'email' => 'jane.doe@example.com',
+            'password' => 'password456qqq',
+        ]);
+
+        $response->assertStatus(401);
+    }
+
+    /**
+     * Testa o acesso às informações do usuário autenticado.
+     *
+     * @return void
+     */
+    public function testUserDetails()
+    {
+        $register = $this->postJson('/api/register', [
+            'name' => 'Jane Doe',
+            'email' => 'jane.doe@example.com',
+            'password' => 'password456',
+        ]);
+
+        $token = $register['token'];
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->getJson('/api/user');
+
+        $response->assertStatus(200);
+    }
+}
